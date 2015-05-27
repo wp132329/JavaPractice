703fbf8159e01bc12e42bf66d3027ef8b667c92a
ARCUS <wlsgur1131@gmail.com>
2015-05-27 14:38:08 +0900

    아이돌 그룹/이름 출력 프로그램 (for문 + for-each문)

---
+++ src/org/dimigo/oop/IdolGruop.java
@@ -0,0 +1,37 @@
+package org.dimigo.oop;
+
+import java.lang.reflect.Member;
+
+public class IdolGruop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] idolGroup = {"빅뱅", "2NE1", "걸스데이"};
+		
+		String[][]  member = {
+				new String[] {"GD", "태양", "대성", "탑", "승리"},
+				new String[] {"CL", "산다라박", "박봄", "민지"},
+				new String[] {"유라", "소진","민아","혜리", "그외"}
+		};
+		
+		// for문
+		for(int i = 0; i<idolGroup.length; i++) {
+			System.out.println("<< " + idolGroup[i]+" 멤버 >>");
+			for(int j = 0; j<member[i].length; j++) {
+				System.out.println(member[i][j]);
+			}
+			System.out.println();
+		}
+		// for-each문
+		for(String name : idolGroup) {
+			for(String[] strarr : member) {
+				System.out.println("<< " + name+" 멤버 >>");
+				for(String name2 : strarr)	{
+					System.out.println(name2);
+				}
+				System.out.println();
+			}
+			
+		}
+	}
+}
