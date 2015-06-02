13679b397e23a41e3fde98a49a48b6b7d7188762
ARCUS <wlsgur1131@gmail.com>
2015-06-02 11:11:55 +0900

    아이돌 그룹명/멤버명 출력 프로그램

---
+++ src/org/dimigo/oop/Idol.java
@@ -0,0 +1,24 @@
+package org.dimigo.oop;
+
+
+public class Idol {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] idolGroup = {"빅뱅", "2NE1", "걸스데이"};
+		
+		String[][]  member = {
+				new String[] {"GD", "태양", "대성", "탑", "승리"},
+				new String[] {"CL", "산다라박", "박봄", "민지"},
+				new String[] {"유라", "소진","민아","혜리", "그외"}
+		};
+		// for문
+		for(int i = 0; i<idolGroup.length; i++) {
+			System.out.println("<< " + idolGroup[i]+" 멤버 >>");
+			for(int j = 0; j<member[i].length; j++) {
+				System.out.println(member[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
