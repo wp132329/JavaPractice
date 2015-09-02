bb3a239f84ab85a37026620a85ecbc7080f90c3c
ARCUS <wlsgur1131@gmail.com>
2015-09-02 12:12:06 +0900

    스마트폰 정보 출력-추상클래스

---
+++ src/org/dimigo/abstractclass/IPhone.java
@@ -0,0 +1,24 @@
+package org.dimigo.abstractclass;
+
+public class IPhone extends SmartPhone {
+
+	public IPhone() {
+		
+	}
+	
+	public IPhone(String model, String company, int price) {
+		super(model,company,price); 
+		
+	}
+	
+	public void pay() {
+		System.out.println("Apple Pay로 결제합니다.");
+	}
+	
+	public void useAirDrop() {
+		System.out.println("AirDrop 기능을 사용합니다.");
+	}
+	
+	
+	
+}
